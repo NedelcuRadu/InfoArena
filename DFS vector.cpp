852ef9a506d6f940664bb2ba39ee1e438ea1e776
ShumaherK <43296487+ShumaherK@users.noreply.github.com>
2019-04-12 11:35:08 +0300

    Update DFS vector.cpp

--- DFS vector.cpp
+++ DFS vector.cpp
@@ -7,17 +7,19 @@ ofstream out("dfs.out");
 int N, M;
 bool viz[100005] = {0};
 vector<int> A[100005];
+
 void DFS(int nod) {
-    viz[nod] = 1;
-    for(int i = 0; i < A[nod].size(); i++)
-        if(!viz[A[nod][i]])
+    viz[nod] = 1; //Marcam nodul ca fiind vizitat
+    for(int i = 0; i < A[nod].size(); i++) //Luam toti vecinii
+        if(!viz[A[nod][i]]) // Daca nu e vizitat il parcurgem
             DFS(A[nod][i]);
 }
+
 int main() {
     in >> N >> M;
     int x, y;
     int cnt = 0;
-    for(int i = 1; i <= M; i++) {
+    for(int i = 1; i <= M; i++) { // Lista de adiacenta
         in >> x >> y;
         A[x].push_back(y);
         A[y].push_back(x);
@@ -26,7 +28,7 @@ int main() {
     for(int i = 1; i <= N; i++)
         if(!viz[i]) {
             DFS(i);
-            cnt++;
+            cnt++; //Crestem nr de componente conexe
         }
     out << cnt;
     return 0;
